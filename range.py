65086d2cca071d44059e46a516b898233f754ef3
Zoia <3196370@gmail.com>
2020-06-08 13:25:09 +0300

    range method

---
+++ range.py
@@ -0,0 +1,7 @@
+my_list = [a for a in range(20, 87)]
+def my_numbers():
+    even_numbers = []
+    for number in my_list:
+        if number % 2 == 0:
+            even_numbers.append(number)
+            
