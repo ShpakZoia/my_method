035d78eb59592675100ea442c3b67c52fa0d1d89
Zoia <3196370@gmail.com>
2020-06-08 13:47:16 +0300

    range method changed

--- range.py
+++ range.py
@@ -4,4 +4,6 @@ def my_numbers():
     for number in my_list:
         if number % 2 == 0:
             even_numbers.append(number)
-            
+            return even_numbers
+
+print(my_numbers())
